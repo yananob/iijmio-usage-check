eb2023a5248e3a7895acce905b3763206755c9fb
yananob <yananob@gmail.com>
2025-03-01 15:25:02 +0900

    rename

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -eu
 
 # choose deploy type
 # bash ./_cf-common/deploy/deploy_php_http.sh . XXXX
-# bash ./_cf-common/deploy/deploy_php_event.sh . XXXX
+bash ./_cf-common/deploy/deploy_php_event.sh . iijmio-usage-checker
