83db49b606fdc2a96dcaa17c62fe74374e7492b2
yananob <yananob@gmail.com>
2026-06-06 14:06:02 +0900

    updates

--- tests/listen_local_event.sh
+++ tests/listen_local_event.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eu
 
+source ./tests/secrets.sh
+source ./_cf-common/test/export_secrets.sh ${SECRETS[*]}
+
 export FUNCTION_TARGET=main_event
 export FUNCTION_SIGNATURE_TYPE=cloudevent
 php -S localhost:8080 vendor/bin/router.php
+
+source ./_cf-common/test/unset_secrets.sh ${SECRETS[*]}
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -eu
 
+source ./tests/secrets.sh
+source ./_cf-common/test/export_secrets.sh ${SECRETS[*]}
+
 echo "Running PHPStan..."
 ./vendor/bin/phpstan analyze -c ./phpstan.neon
 
 ./vendor/bin/phpunit --colors=auto --display-notices --display-warnings tests/
 # ./vendor/bin/phpunit tests/MyHelloTest.php
+
+source ./_cf-common/test/unset_secrets.sh ${SECRETS[*]}
